977b3b8cb158b91b2358b3fef0d76d5a1147afa1
Ian Seal <iantheging@gmail.com>
2018-02-19 20:50:00 -0600

    Changed Tracker, added Activity and Entry to subdivide better

--- src/first_attempt/Tracker.java
+++ src/first_attempt/Tracker.java
@@ -1,128 +1,60 @@
 package first_attempt;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class Tracker {
 
-    private double milesBiked;
-    private double kmRowed;
-    private double lbsWeight;
-    private int month;
-    private int day;
-
+    private ArrayList<Entry> track;
 
     /**
-     * @param b how many miles biked
-     * @param r how many kilometers rowed
-     * @param w weight for that day
+     * Default contructor with a blank ArrayList
+     */
+    public Tracker() {
+        track = new ArrayList<>();
+    }
+
+    /**
+     * Reads in a previous Tracker
+     * @param filename
+     * @throws FileNotFoundException
+     */
+    public Tracker(String filename) throws FileNotFoundException {
+        // TODO
+    }
+
+    /**
+     * Adds a new entry to track
+     *
+     * @param m month
+     * @param d day
+     * @param w weight
+     * @param c count
+     * @param a activity
+     */
+    public void addEntry(int m, int d, int w, double c, Activity a) {
+        track.add(new Entry(m, d, w, c, a));
+    }
+
+    /**
+     * Gets all entries for a certain day by checking track for correct day
+     *
      * @param m month
      * @param d day
      */
-    public Tracker(double b, double r, double w, int m, int d) {
-        milesBiked = b;
-        kmRowed = r;
-        lbsWeight = w;
-        month = m;
-        day = d;
-    }
-
-    public double getMilesBiked() {
-        return milesBiked;
-    }
-
-    public double getKmRowed() {
-        return kmRowed;
-    }
-
-    public double getLbsWeight() {
-        return lbsWeight;
+    public void getEntriesDay(int m, int d) {
+        for (int i = 0; i < track.size(); i++) {
+            if (track.get(i).getMonth() == m && track.get(i).getDay() == d) {
+                track.get(i).prntStr();
+            }
+        }
     }
 
     /**
-     * Prints the date numerically
-     *
-     * @return string in MM/dd format
+     * Transfers all entries to a spreadsheet
      */
-    public String getDateNumeral() {
-        return month + "/" + day;
-    }
-
-    @Override
-    public String toString() {
-        String monthName;
-        String dayName;
-        switch (month) {
-            case 1:
-                monthName = "January";
-                break;
-
-            case 2:
-                monthName = "February";
-                break;
-
-            case 3:
-                monthName = "March";
-                break;
-
-            case 4:
-                monthName = "April";
-                break;
-
-            case 5:
-                monthName = "May";
-                break;
-
-            case 6:
-                monthName = "June";
-                break;
-
-            case 7:
-                monthName = "July";
-                break;
-
-            case 8:
-                monthName = "August";
-                break;
-
-            case 9:
-                monthName = "September";
-                break;
-
-            case 10:
-                monthName = "October";
-                break;
-
-            case 11:
-                monthName = "November";
-                break;
-
-            case 12:
-                monthName = "December";
-                break;
-
-            default:
-                monthName = "invalid month selected";
-                break;
-
-        }
-
-        switch (day) {
-            case 1:
-                dayName = "1st";
-                break;
-
-            case 2:
-                dayName = "2nd";
-                break;
-
-            case 3:
-                dayName = "3rd";
-                break;
-
-            default:
-                dayName = day + "th";
-                break;
-        }
-
-        return monthName + " the " + dayName;
+    public void writeToSpreadsheet() {
+        // TODO
     }
 
 }
